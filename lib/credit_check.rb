c15d2dd88b80703ae3bf82b389753bda4f27adbc
Pat <pat.mccartney2@gmail.com>
2018-06-28 20:54:56 -0600

    final commit. full functionality through iteration 4

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -2,32 +2,99 @@ require 'pry'
 
 class CreditCheck
 
+  attr_reader :og_card_number,
+              :doubled_card_number,
+              :mutated_card_number,
+              :check_sum_array
 
-  attr_reader :ccnumber
+   def initialize
+     @og_card_number = []
+     @mutated_card_number = []
+     @doubled_card_number = []
+     @check_sum_array = []
+     terminal_interface
+   end
 
+   def take_card_and_split(card_number)
+     @og_card_number = card_number.digits
+   end
 
-  def initialize
-    @ccnumber = []
-  end
+   def double_every_other_number
+     @og_card_number.each_with_index do |number, index|
+       if index.odd? == true
+        @doubled_card_number << number * 2
+       else
+        @doubled_card_number << number
+       end
+     end
+     return @doubled_card_number
+   end
 
-  def valid?(card_number)
-    ccarray = card_number.digits
-    ccarray.map.with_index do |number, index|
-      if (index + 1) % 2 == 0
-        number * 2
-      else
-        number
-      end
+   def get_rid_of_over_ten
+     @doubled_card_number.each do |number|
+       if number > 9
+        @mutated_card_number << number - 9
+       else
+        @mutated_card_number << number
+       end
+     end
+     return @mutated_card_number
+   end
+
+   def validity?
+     if @mutated_card_number.sum % 10 == 0
+       true
+     else
+       false
+     end
+   end
+
+   def valid_number?(card_number)
+     take_card_and_split(card_number)
+     double_every_other_number
+     get_rid_of_over_ten
+     validity?
+   end
+
+   def validation_output(cardnumber)
+     if valid_number?(cardnumber) == true
+       print "The number #{cardnumber} is valid \n"
+     else
+       print "The number #{cardnumber} is invalid \n"
+     end
+   end
+
+   def get_check_digit(cardnumber)
+     mutated = []
+     @check_sum_array = cardnumber.digits.reverse
+     @check_sum_array.each_with_index do |number, index|
+       if (index + 1) % 2 == 0
+        mutated << number * 2
+       else
+         mutated << number
+       end
+     end
+     result = []
+     mutated.each do |number|
+       if number > 9
+         result << number - 9
+       else
+         result << number
+       end
+     end
+     array_sum = result.sum * 9
+     array_sum % 10
+   end
+
+   def terminal_interface
+     puts "Card to be validated?"
+     input_2 = gets.chomp
+        while input_2 != "n"
+     user_input = gets.chomp.to_i
+     validation_output(user_input)
+     print "Another Card to be Verified? y/n \n"
+     input_2 = gets.chomp
     end
-    ccarray.map do |number|
-      if number > 10
-        number - 9
-      end
-    end
-    
-  end
-
-
-
-
-end
+    abort("You're Cards are either Invalid or Valid.  Now You know, and Knowing is half the battle.")
+   end
+ end
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -6,16 +6,65 @@ require 'pry'
 class CreditCheckTest < Minitest::Test
 
   def test_existence
-    cc = CreditCheck.new
-    assert_instance_of CreditCheck, cc
+    credit_check = CreditCheck.new
+    assert_instance_of CreditCheck, credit_check
   end
 
 
- def test_it_can_create_doublecc
-   cc = CreditCheck.new
-   num = cc.valid?(1234599)
-   assert_equal [9, 18, 5, 8, 3, 4, 1], num
+ def test_it_can_save_and_split_card
+   credit_check = CreditCheck.new
+   result = credit_check.take_card_and_split(12345)
+   assert_equal [5, 4, 3, 2, 1], result
  end
 
+ def test_it_can_double_every_other_number
+   credit_check = CreditCheck.new
+   credit_check.take_card_and_split(12345)
+   result = credit_check.double_every_other_number
+   assert_equal [5, 8, 3, 4, 1], result
+ end
 
+ def test_it_can_deal_with_nums_over_10
+   credit_check = CreditCheck.new
+   credit_check.take_card_and_split(888)
+   credit_check.double_every_other_number
+   credit_check.get_rid_of_over_ten
+   assert_equal [8, 7, 8], credit_check.mutated_card_number
+ end
+
+ def test_it_can_check_valid_cards
+   credit_check = CreditCheck.new
+   credit_check.take_card_and_split(4024007136512380)
+   credit_check.double_every_other_number
+   credit_check.get_rid_of_over_ten
+   result = credit_check.validity?
+   assert_equal true, result
+ end
+
+ def test_it_can_get_invalid_cards
+  credit_check = CreditCheck.new
+  credit_check.take_card_and_split(541801923795240)
+  credit_check.double_every_other_number
+  credit_check.get_rid_of_over_ten
+  result = credit_check.validity?
+  assert_equal false, result
+ end
+
+ def test_whole
+  credit_check = CreditCheck.new
+  result = credit_check.valid_number?(541801923795240)
+  assert_equal false, result
+ end
+
+ def test_outputs
+   credit_check = CreditCheck.new
+   result = credit_check.validation_output(5541808923795240)
+   assert_equal "The number 5541808923795240 is valid", result
+ end
+
+ def test_check_digit
+   credit_check = CreditCheck.new
+   result = credit_check.get_check_digit(7992739871)
+   assert_equal 3, result
+ end
 end
